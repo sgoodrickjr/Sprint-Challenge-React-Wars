69db5542e066da9b877db29fd3821f9c828e5b53
Stephen Goodrick <sgoodrickjr@gmail.com>
2020-05-02 12:46:49 -0600

    styled names

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Character from "./components/Character";
 import './App.css';
+import axios from "axios";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Button } from 'reactstrap';
+
+console.log(Button);
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
+    const [characterList, setCharacterList] = useState([]);
+
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  useEffect(() => {
+    axios
+      .get("https://rickandmortyapi.com/api/character/")
+      .then(response => {
+       // console.log("Our axios request", response.data.results);
+        setCharacterList(response.data.results);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }, []);
+ // console.log("our character list", characterList);
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Button color="link">Button</Button>
+      {characterList.map(information => {
+        return (
+         <Character characterList={information}/> 
+        )
+      })
+      }
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 // Write your Character component here
+
+
+
+const Character = (props) => {
+   // console.log("props from App.js", props);
+    const [name, setName] = useState([]);
+    return (
+        <div className="characters">
+            <h2
+
+            style={{ fontSize: "12px"}, {color: "white"}}
+            
+
+            >{props.characterList.name}</h2>
+        </div>
+    )
+}
+
+
+export default Character;
